8c5e8623a0d571f456de779c4fa0bcdc40f171f1
Souvik Dutta <souvikdutta5122005@gmail.com>
2025-02-23 20:29:27 +0530

    Make Login ui

---
+++ frontend/src/routes/AppRoutes.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Route,BrowserRouter,Routes } from 'react-router-dom'
+import Login from '../screens/login'
+
+
+
+
+const AppRoutes = () => {
+    
+  return (
+    <BrowserRouter>
+        <Routes>
+            <Route path='/' element={<div>Home</div>} />
+            <Route path='/login' element={<Login />}/>
+            <Route path='/register' element={<div>Register</div>} />
+        </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default AppRoutes
